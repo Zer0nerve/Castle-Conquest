b4b326d99cf118adf0ddb4a4b408face14635c3b
Zer0nerve <itodd30@gmail.com>
2022-04-30 22:06:12 -0600

    controls added
    Tutorials

---
+++ Castle Conquest/Assets/Scripts/TestingInputSystem.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class TestingInputSystem : MonoBehaviour
+{
+
+    private Rigidbody2D Player;
+    private PlayerInput playerInput;
+    private PlayerInputActions playerInputActions;
+
+    private void Awake()
+    {
+        Player = GetComponent<Rigidbody2D>();
+        playerInput = GetComponent<PlayerInput>();
+
+        playerInputActions = new PlayerInputActions();
+        playerInputActions.Player.Enable();
+        playerInputActions.Player.Jump.performed += Jump;
+        playerInputActions.Player.Movement.performed += Movement_performed;
+
+
+
+    }
+
+    private void Update()
+    {
+        
+        Vector2 inputVector = playerInputActions.Player.Movement.ReadValue<Vector2>();
+        float speed = 3f;
+        Player.AddForce(new Vector3(inputVector.x, 0, inputVector.y) * speed, ForceMode2D.Force);
+    }
+    private void Movement_performed(InputAction.CallbackContext context)
+    {
+        Debug.Log(context);
+        Vector2 inputVector = context.ReadValue<Vector2>();
+        float speed = 3f;
+        Player.AddForce(new Vector3(inputVector.x, 0, inputVector.y) * speed, ForceMode2D.Force);
+    }
+
+    public void Jump(InputAction.CallbackContext context)
+    {
+        Debug.Log(context);
+        if (context.performed)
+        { 
+        Debug.Log("Jump!" + context.phase);
+        Player.AddForce(Vector3.up * 5f, ForceMode2D.Impulse);
+    }
+    }
+}
